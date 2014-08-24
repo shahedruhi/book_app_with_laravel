850be12b9bb2d34c5314b878aef4988582d0ef70
shahedruhi <shahedruhi@gmail.com>
2014-08-24 14:51:56 +0600

    index page

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,7 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::resource('books','BookController');
+
+//Route::controller('/', 'BookController');
---
+++ app/views/layouts/book.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+    <title>George's BookShop::Peru</title>
+    <meta charset="utf-8">
+    <link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'>
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <style>
+        table form { margin-bottom: 0; }
+        form ul { margin-left: 0; list-style: none; }
+        .error { color: red; font-style: italic; }
+        body { padding-top: 20px; }
+    </style>
+</head>
+<body>
+<div class="container">
+    @if (Session::has('message'))
+    <div class="flash alert">
+        <p>{{ Session::get('message') }}</p>
+    </div>
+    @endif
+
+    @yield('main')
+</div>
+</body>
+</html>
